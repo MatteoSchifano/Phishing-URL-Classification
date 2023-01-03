03fc50c097cac74ac62f39a263e815178a1f7c42
MatteoSchifano <matvinsch@gmail.com>
2023-01-03 21:22:07 +0100

    tuning phishing

--- main_phishing.py
+++ main_phishing.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.33)
     
-    clf = RandomForestClassifier()
+    clf = RandomForestClassifier(max_depth=20, min_samples_split=5)
     clf.fit(X_train, y_train)
     
     pred = clf.predict(X_test)
